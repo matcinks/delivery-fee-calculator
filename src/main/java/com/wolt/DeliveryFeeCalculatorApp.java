65770606b6301889128a78d9d9005771624e58d3
matcinks <x.mateuszstepien@gmail.com>
2024-01-28 18:28:26 +0100

    added spring boot

--- src/main/java/com/wolt/DeliveryFeeCalculatorApp.java
+++
@@ -1,7 +0,0 @@
-package com.wolt;
-
-public class DeliveryFeeCalculatorApp {
-    public static void main(String[] args) {
-        System.out.println("Hello, World");
-    }
-}
---
+++ src/main/java/com/wolt/domain/DeliveryFeeCalculatorApp.java
@@ -0,0 +1,11 @@
+package com.wolt.domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryFeeCalculatorApp {
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryFeeCalculatorApp.class, args);
+    }
+}
